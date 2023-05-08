3d4864e62da206923f5b2093aae6b059dc4afafc
domenicolorenti <domenicolorenti.dev@gmail.com>
2023-05-08 19:47:14 +0200

    sidebar fixed

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,18 @@ import AppBar from './components/AppBar/AppBar';
 import SideBar from './components/SideBar/SideBar';
 
 export default function App() {
-  const [sideBarClass, setSideBarClass] = useState("");
+  const [sideBarClass, setSideBarClass] = useState("transform translate-x-full");
   const [sideBarEnabled, setSideBarEnabled] = useState(false);
-  const [userLogged, setUserLogged] = useState(true)
-
-  let sidebarChange = false;
+  const [userLogged, setUserLogged] = useState(true);
+  const [sidebarChange, setSidebarChange] = useState(false);
 
   useEffect(() => {
-    if(sideBarEnabled && sidebarChange)
+    if(sideBarEnabled) {
         setSideBarClass("transform -translate-x-50");
-    else {
-        setSideBarClass("transform translate-x-full");
-        sidebarChange = true;
+        setSidebarChange(true);
     }
+    else if(!sideBarEnabled && sidebarChange)
+      setSideBarClass("transform translate-x-full");
 
 }, [sideBarEnabled]);
 
