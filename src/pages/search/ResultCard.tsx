5db58095b06a4e560af01a7c2a0d81e98565a65f
Domenico Lorenti <domenicolorenti.dev@gmail.com>
2023-06-12 09:38:52 +0200

    feat: search page

--- src/pages/search/ResultCard.tsx
+++ src/pages/search/ResultCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { Rating } from '@mui/material'
 
 const ResultCard = (props: any) => {
   return (
@@ -8,9 +8,12 @@ const ResultCard = (props: any) => {
         src="https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cmVzdGF1cmFudHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80"
         alt=""
       />
-      <div className="w-2/3 justify-center p-4">
-        <h1 className="text-2xl">Rensturant Name</h1>
-        <h3>Rensturant Via</h3>
+      <div className="flex flex-col w-2/3 justify-center p-6">
+        <h1 className="text-3xl">{props.name}</h1>
+        <h3 className="text-xl">{props.address}</h3>
+        <h4 className="my-2">{props.description}</h4>
+        <Rating name="read-only" value={props.value} readOnly />
+        <button className="bg-gray-800 rounded-xl shadow text-xl text-white px-8 py-2 w-1/2 mx-auto mt-auto focus:outline-none">Show details</button>
       </div>
     </div>
   )
--- src/pages/search/Search.tsx
+++ src/pages/search/Search.tsx
@@ -4,6 +4,7 @@ import { useLocation } from 'react-router-dom'
 import ResultCard from './ResultCard';
 import { APIManager } from '../../api/APIManager';
 import { APIConfig } from '../../api/APIConfig';
+import { Results } from './Results';
 
 const Search = () => {
   const [result, setResult] = useState([]);
@@ -39,24 +40,27 @@ const Search = () => {
     <div className="flex flex-col justify-center items-center w-full my-8">
       <form className="flex flex-row w-1/2 p-2 m-4 border rounded-2xl ">
         <input
-          className="w-full mx-2 focus:outline-none"
+          className="w-full mx-2 p-2 text-xl focus:outline-none"
           type="text"
           placeholder="Search"
         />
-        <button className="bg-gray-800 rounded-xl shadow px-6 py-2 ml-auto focus:outline-none" type="submit" onSubmit={handleSubmit}>
-          <HiSearch className="text-xl text-white" />
+        <button className="bg-gray-800 rounded-xl shadow px-8 py-2 ml-auto focus:outline-none" type="submit" onSubmit={handleSubmit}>
+          <HiSearch className="text-2xl text-white" />
         </button>
       </form>
       <div className="flex flex-col w-2/3 p-4 justify-center items-center">
-        <ResultCard />
-        <ResultCard />
-        <ResultCard />
-        <ResultCard />
-        <ResultCard />
-        <ResultCard />
-        <ResultCard />
-        <ResultCard />
-        <ResultCard />
+        {(
+          <>
+            {Results.map((item) => (
+              <ResultCard
+                name={item.name}
+                address={item.address}
+                description={item.description}
+                value={item.value}
+              />
+            ))}
+          </>
+        )}
       </div>
     </div>
   )
